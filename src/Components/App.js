1b4dc0b694c465d595af56c876933423561a9e1d
Whitney Perricone <whitney.perricone@gmail.com>
2022-07-09 15:15:32 -0600

    Add banner image

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,9 @@
-import '../Styles/App.css';
 import { useQuery, gql } from '@apollo/client';
 import './NavBar';
 import NavBar from './NavBar';
+import styled from 'styled-components';
+
+
 
 // const GET_LOCATIONS = gql`
 //   query GetLocations {
@@ -36,8 +38,16 @@ function App() {
   return (
     <div>
       <NavBar/>
+      <Img src={require ("../assets/myrlene-numa-SnITZTTeJVE-unsplash.jpg")}/>
+      <div>
+      Placeholder for Craft Container
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const Img = styled.img`
+width: 100vw;
+`;
