1c5cb7367bddcb0809ed79126e125eebe018b344
Whitney Perricone <whitney.perricone@gmail.com>
2022-07-09 14:13:33 -0600

    Cleanup files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './Components/App';
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -22,7 +21,3 @@ root.render(
   </ApolloProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
