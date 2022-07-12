7e5e84ab06bedf0cb4244a94400a956ef635a0ac
Whitney Perricone <whitney.perricone@gmail.com>
2022-07-11 18:25:27 -0600

    Add preliminary tests to the landing page via cypress

---
+++ cypress/e2e/1-getting-started/landingPage.cy.js
@@ -0,0 +1,28 @@
+describe('Craft Circle landing page view', () => {
+  beforeEach(() => {
+    cy.intercept("craft-circle-be.herokuapp.com/graphql", {fixture: "userProfile.json"})
+    cy.visit("http://localhost:3000")
+  });
+
+  it('Should have a Nav Bar', () => {
+    cy.get('.nav-bar')
+    .should('exist')
+  });
+
+  it('Should have two buttons', () => {
+    cy.get('.nav-button')
+    .eq(0)
+    .contains('All Crafts')
+    cy.get('.nav-button')
+    .eq(1)
+    .contains('Profile')
+  });
+
+  it('Should have a search bar', () => {
+    cy.get('.search-bar')
+    cy.get('.search-button')
+  })
+
+
+}
+)
