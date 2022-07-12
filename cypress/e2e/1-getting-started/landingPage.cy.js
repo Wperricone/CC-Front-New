164c0393fc3f537bb66e8c97934830b79a5a1388
Joe Rupp <joerpp8@gmail.com>
2022-07-12 17:10:21 -0600

    Adjust cypress tests
    Testing has been added to no longer include features that have been moved

--- cypress/e2e/1-getting-started/landingPage.cy.js
+++ cypress/e2e/1-getting-started/landingPage.cy.js
@@ -1,37 +1,16 @@
-describe('Craft Circle landing page view', () => {
+describe("Craft Circle landing page view", () => {
   beforeEach(() => {
     //cy.intercept("craft-circle-be.herokuapp.com/graphql", {fixture: "userProfile.json"})
-    cy.visit("http://localhost:3000")
+    cy.visit("http://localhost:3000");
   });
 
-  it('Should have a Nav Bar', () => {
-    cy.get('.nav-bar')
-    .should('exist')
+  it("Should have a Nav Bar", () => {
+    cy.get(".nav-bar").should("exist");
   });
 
-  it('Should have three buttons', () => {
-    cy.get('.nav-button')
-    .eq(0)
-    .contains('All Crafts')
-    cy.get('.nav-button')
-    .eq(1)
-    .contains('About')
-    cy.get(".nav-button")
-    .eq(2)
-    .contains("Profile")
+  it("Should have three buttons", () => {
+    cy.get(".nav-button").eq(0).contains("All Crafts");
+    cy.get(".nav-button").eq(1).contains("About");
+    cy.get(".nav-button").eq(2).contains("Profile");
   });
-
-  it('Should have an About section', () => {
-    cy.get('.search-and-about-container')
-    cy.get('h2')
-    .contains('About')
-  });
-
-  it('Should have a search bar', () => {
-    cy.get('.search-bar')
-    cy.get('.search-button')
-  });
-
-
-}
-)
+});
