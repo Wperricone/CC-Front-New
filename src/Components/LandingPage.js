6ba801dd7024c6948381addb9391161bb834bff6
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-07-11 19:53:51 -0600

    Add landing page image and styling to match wireframe

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -4,23 +4,49 @@ import styled from 'styled-components';
 
 const LandingPage = () => {
     return (
-        <section className="landing-page">
-            <img className="landing-page-img" src="" alt="crafts-in-action"/>
-            <div className="search-and-about-container">
-                <h2>About</h2>
-                <p>about me goes here</p>
+        <LandingPageSection className="landing-page">
+            <LandingImage className="landing-page-img" src={require ("../assets/bannerImage.png")} alt="crafts-in-action"/>
+            <Contents className="search-and-about-container">
+                <About>
+                    <h2>About</h2>
+                 <p>about me goes here</p>
+                </About>
                 <form className="search-bar">
                     <label>Search</label>
                     <input type="text"></input>
                     <button>search</button>
                 </form>
-            </div>
-        </section>
+            </Contents>
+        </LandingPageSection>
     )
 }
 
 export default LandingPage
 
+const LandingImage = styled.img`
+width: 80vw;
+border-radius: 30px;
+box-shadow: 40px 40px rgba(239,203, 199, 1);
+margin: 10px;
+`
 
+const LandingPageSection = styled.section`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+const About = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+const Contents = styled.div`
+display: flex;
+flex-direction: row;
+border: solid 1px black;
+width: 80vw;
+justify-content: space-between;
+align-items: center;
+`
 //might need to play with divs to get felx box to match wireframe 
 // feel free to change structure/ verbiage of search bar 
