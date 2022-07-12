04399eb0bbcb345a699ae80e0927ed028ce7340a
Joe Rupp <joerpp8@gmail.com>
2022-07-12 13:55:15 -0600

    Add error handling
    Add 404 error page and routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,6 +6,10 @@ import styled from "styled-components";
 import { Route, Routes } from "react-router-dom";
 import AboutPage from "./AboutPage";
 import UserProfile from "./UserProfile";
+import Form from "./Form";
+import ItemExpanded from "./ItemExpanded";
+import Login from "./Login";
+import NoMatch from "./NoMatch";
 
 function App() {
   return (
@@ -13,8 +17,13 @@ function App() {
       <NavBar />
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/about" element={<AboutPage />} />
-        <Route exact path="/profile" element={<UserProfile />} />
+        <Route exact path="about" element={<AboutPage />} />
+        <Route exact path="profile" element={<UserProfile />} />
+        <Route exact path="contribution" element={<Form />} />
+        <Route exact path="craft/:craftId" element={<ItemExpanded />} />
+        {/* <Route exact path="confirmation" element={} /> */}
+        <Route exact path="login" element={<Login />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </main>
   );
