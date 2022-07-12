f665026e110ed559ba37502297691a1228a43405
Whitney Perricone <whitney.perricone@gmail.com>
2022-07-11 19:25:13 -0600

    Add preliminary testing to item expanded via Cypress

---
+++ cypress/e2e/1-getting-started/itemExpandedView.cy.js
@@ -0,0 +1,28 @@
+describe('Craft Circle landing page view', () => {
+  beforeEach(() => {
+    cy.intercept("craft-circle-be.herokuapp.com/graphql", {fixture: "userProfile.json"})
+    cy.visit("http://localhost:3000")
+  });
+
+  it('Should have an image and a button', () => {
+    cy.get('.item-image-large')
+    .should('exist')
+    cy.get('.contact-button')
+    .contains('Contact Crafter')
+  });
+
+  it('Should hav four different pieces of information', () => {
+    cy.get('h2')
+    .should('exist')
+    cy.get('h3')
+    .should('exist')
+    cy.get('p')
+    .eq(0)
+    .should('exist')
+    cy.get('p')
+    .eq(1)
+    .should('exist')
+  })
+
+}
+)
