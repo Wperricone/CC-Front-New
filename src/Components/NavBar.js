0e9a2ede8ebf1e97ade7f8617bdb71fcb11fbbee
Joe Rupp <joerpp8@gmail.com>
2022-07-12 14:03:59 -0600

    Add links to the navigation bar
    The user can now click the buttons in the nav bar to move throughout the app

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,45 +1,55 @@
-import React from 'react';
-import styled from 'styled-components';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 const NavBar = () => {
-	return (
-		<Nav className='nav-bar'>
-			<Img className="logo" src={require ("../assets/CraftCircleLogo-06.png")} alt="craft circle logo"/>
-			<div>
-				<Button className='nav-button'>
-					<h3>All Crafts</h3>
-				</Button>
-				<Button className="nav-button">
-					<h3>About</h3>
-				</Button>
-				<Button className="nav-button">
-					<h3>Profile</h3>
-				</Button>
-			</div>
-		</Nav>
-	)
-}
+  return (
+    <Nav className="nav-bar">
+      <Img
+        className="logo"
+        src={require("../assets/CraftCircleLogo-06.png")}
+        alt="craft circle logo"
+      />
+      <div>
+        <NavLink to={"/"}>
+          <Button className="nav-button">
+            <h3>All Crafts</h3>
+          </Button>
+        </NavLink>
+        <NavLink to={"about"}>
+          <Button className="nav-button">
+            <h3>About</h3>
+          </Button>
+        </NavLink>
+        <NavLink to={"profile"}>
+          <Button className="nav-button">
+            <h3>Profile</h3>
+          </Button>
+        </NavLink>
+      </div>
+    </Nav>
+  );
+};
 
 export default NavBar;
 
 const Button = styled.button`
-background: #F4ECDD;
-border: none;
-font-size: 25px;
-color: #A7C891;
-margin: 25px;
-&: hover {
-	color: #E3976E
-} 
+  background: #f4ecdd;
+  border: none;
+  font-size: 25px;
+  color: #a7c891;
+  margin: 25px;
+  &: hover {
+    color: #e3976e;
+  }
 `;
 
 const Nav = styled.nav`
-display: flex;
-justify-content: space-between;
-padding: 25px;
+  display: flex;
+  justify-content: space-between;
+  padding: 25px;
 `;
 
 const Img = styled.img`
-height: 150px;
-`
+  height: 150px;
+`;
