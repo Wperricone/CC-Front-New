8336b8c73312ea0d78e844490a14e976a5590028
Joe Rupp <joerpp8@gmail.com>
2022-07-12 22:52:04 -0600

    Add carousel indicator
    Added styling and partial functionality to image image indicator

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Carousel = () => {
+  const [currentImage, setImage] = useState("bannerImage.png");
+
   return (
     <div>
       <LandingImage
         className="landing-page-img"
-        src={require("../assets/bannerImage.png")}
+        src={require(`../assets/${currentImage}`)}
         alt="crafts-in-action"
       />
+      <IndicatorBar>
+        <Indicator onClick={() => setImage("bannerImage.png")}></Indicator>
+        <Indicator onClick={() => setImage("bannerImage.png")}></Indicator>
+        <Indicator onClick={() => setImage("bannerImage.png")}></Indicator>
+      </IndicatorBar>
     </div>
   );
 };
@@ -16,8 +23,26 @@ const Carousel = () => {
 export default Carousel;
 
 const LandingImage = styled.img`
-  width: 80vw;
+  width: 85vw;
   border-radius: 25px;
-  box-shadow: 40px 40px rgba(239, 203, 199, 1);
+  box-shadow: 30px 30px rgba(239, 203, 199, 1);
   margin: 10px;
 `;
+
+const IndicatorBar = styled.div`
+  margin-top: 3%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Indicator = styled.div`
+  width: 15px;
+  height: 15px;
+  margin: 2%;
+  border-radius: 25px;
+  background: #b7c1dc;
+`;
+
+// .active & {
+//   background: #a7c891;
+// }
