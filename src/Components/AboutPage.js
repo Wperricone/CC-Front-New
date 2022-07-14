83040a03f2548989d754e2d65000f727397fe7e8
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-07-13 21:45:28 -0600

    Add image assets for About page, change text styling to align with wireframe

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -5,8 +5,9 @@ import colors from "../constants/colors";
 const AboutPage = () => {
   return (
     <Contents className="search-and-about-container">
+      <BackgroundImgContainer>
       <About>
-        <AboutH2>About</AboutH2>
+        <AboutH2>we are <H2Highlight>Crafters</H2Highlight></AboutH2>
         <AboutP>
           Craft Circle is an online community where crafters can borrow and
           donate supplies in an effort to make creative outlets more accessible.
@@ -14,6 +15,14 @@ const AboutPage = () => {
           the Denver Metro area.
         </AboutP>
       </About>
+      <JoinCTAContainer>
+
+      </JoinCTAContainer>
+      </BackgroundImgContainer>
+      <Footer>
+        <FooterImg src={require("../assets/bannerImages/bannerImage4.jpg")} alt="water color palette" />
+        <FooterLogo src={require("../assets/CraftCircleLogo-06.png")} alt="craft circle logo"/>
+      </Footer>
     </Contents>
   );
 };
@@ -21,12 +30,16 @@ const AboutPage = () => {
 export default AboutPage;
 
 const AboutH2 = styled.h2`
-  color: ${colors.craftPurple};
-  font-size: 30px;
+  color: ${colors.craftGrey};
+  font-size: 60px;
+  margin-bottom: 20px;
+  font-weight: 700;
 `;
 const AboutP = styled.p`
-  color: ${colors.craftPurple};
-  font-sizr: 15px;
+  color: ${colors.craftGrey};
+  font-size: 15px;
+  font-weight: 500;
+  margin-bottom: 60px;
 `;
 
 const About = styled.div`
@@ -37,9 +50,36 @@ const About = styled.div`
 
 const Contents = styled.div`
   display: flex;
-  flex-direction: row;
-  width: 80vw;
+  flex-direction: column;
+  width: 100vw;
   justify-content: space-between;
   align-items: center;
   margin-top: 30px;
 `;
+const H2Highlight = styled.span`
+  color: ${colors.craftPurple};
+  margin-left: 20px;
+`
+
+const Footer = styled.div`
+  width: 100vw;
+  height: 30vh;
+`
+
+const FooterLogo = styled.img`
+  height: 150px;
+`
+
+const FooterImg = styled.img`
+  width: 100%;
+  height: auto;
+`
+
+const JoinCTAContainer = styled.div`
+  height: 90vh
+`
+const BackgroundImgContainer = styled.div`
+  height: 150vh;
+  border: solid 1px green;
+  background-image: url("../assets/bannerImages/backgroundImage1.jpg)
+`
