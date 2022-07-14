ce131c782794d93a22fd2fd6375180f4b8ddc9f9
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-07-13 19:12:28 -0600

    Adjust order of properties

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,11 +1,11 @@
 const colors = {
-  craftBlack: "#221F20",
-  craftGrey: "#A18E96",
   craftOrange: "#E3976E",
   craftPurple: "#AD92C0",
   craftBlue: "#B7C1DC",
   craftGreen: "#A7C891",
   craftPink: "#EFCBC7",
+  craftBlack: "#221F20",
+  craftGrey: "#A18E96",
   craftWhite: "#F4ECDD"
 }
 
