4d1644f0636b9e3344d8bca4f87c02df6b003fa6
Joe Rupp <joerpp8@gmail.com>
2022-07-14 17:44:41 -0600

    Add search bar
    Added search bar and title for craft display section

--- src/Components/ItemContainer.js
+++ src/Components/ItemContainer.js
@@ -21,9 +21,18 @@ const ItemContainer = ({ itemData }) => {
   });
 
   return (
-    <ItemContainerSection className="item-container">
-      {items}
-    </ItemContainerSection>
+    <>
+      <SectionInfo>
+        <SectionTitle>Crafts:</SectionTitle>
+        <SearchDiv>
+          <SectionTitle>Search:</SectionTitle>
+          <SearchInput></SearchInput>
+        </SearchDiv>
+      </SectionInfo>
+      <ItemContainerSection className="item-container">
+        {items}
+      </ItemContainerSection>
+    </>
   );
 };
 
@@ -37,4 +46,34 @@ const ItemContainerSection = styled.section`
   gap: 5%;
   justify-items: center;
   align-items: center;
+  margin-bottom: 10vh;
+`;
+
+const SectionInfo = styled.div`
+  width: 85vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const SearchDiv = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SectionTitle = styled.h1`
+  color: ${colors.craftPurple};
+  font-size: 35px;
+  font-weight: 900;
+  margin-bottom: 0;
+  margin-top: 5vh;
+  margin-right: 10%;
+`;
+
+const SearchInput = styled.input`
+  height: 35px;
+  width: 15vw;
+  margin-top: 5vh;
+  border-radius: 25px;
+  border: 0;
 `;
