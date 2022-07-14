1d2ef12ffe4957586cbd451e6fa6058aeab05cc7
Joe Rupp <joerpp8@gmail.com>
2022-07-13 19:21:07 -0600

    Add basic category box structure

--- src/Components/CraftCategories.js
+++ src/Components/CraftCategories.js
@@ -2,7 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const CraftCategories = () => {
-  return <div></div>;
+  return (
+    <section>
+      <CategoryBox style={{ background: "blue" }}></CategoryBox>
+    </section>
+  );
 };
 
 export default CraftCategories;
+
+const CategoryBox = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 25px;
+`;
