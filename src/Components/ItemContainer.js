b5c7083533ad0f4ee1055297d9559d3660b64ab0
Joe Rupp <joerpp8@gmail.com>
2022-07-14 17:20:41 -0600

    Refactor craft item styling
    Adjusted layout positions of item cards and add hover/onclick effects

--- src/Components/ItemContainer.js
+++ src/Components/ItemContainer.js
@@ -1,40 +1,40 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import React from "react";
-import ItemCard from './ItemCard';
-import colors from '../constants/colors';
+import ItemCard from "./ItemCard";
+import colors from "../constants/colors";
 
 const ItemContainer = ({ itemData }) => {
+  const randomizeColor = () => {
+    return Object.keys(colors)[Math.floor(Math.random() * 5)];
+  };
 
-   const randomizeColor = () => {
-      return Object.values(colors)[Math.floor(Math.random() * 5)]
-   }
-  
-   const items = itemData.map(item => {
-      return (
-         <ItemCard
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            amount={item.amount}
-            color={randomizeColor()}
-         />
-      );
-   });
+  const items = itemData.map((item) => {
+    return (
+      <ItemCard
+        key={item.id}
+        id={item.id}
+        name={item.name}
+        amount={item.amount}
+        color={randomizeColor()}
+      />
+    );
+  });
 
-   return (
-      <ItemContainerSection className="item-container">
-         {items}
-      </ItemContainerSection>
-   );
-}
+  return (
+    <ItemContainerSection className="item-container">
+      {items}
+    </ItemContainerSection>
+  );
+};
 
 export default ItemContainer;
 
 const ItemContainerSection = styled.section`
-padding: 60px;
-display: grid;
-grid-template-columns: repeat(2, 1fr);
-gap: 60px;
-justify-items: center;
-align-items: center;
+  width: 90vw;
+  padding: 5vw;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 5%;
+  justify-items: center;
+  align-items: center;
 `;
