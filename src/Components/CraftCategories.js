cafd7cca581c71475ed14d5eefcbace9ac0bf265
Joe Rupp <joerpp8@gmail.com>
2022-07-13 20:41:55 -0600

    Add category button styling
    All category icons have been centered and adjusted to be the correct size on a grid, allowing for future responsive design changes

--- src/Components/CraftCategories.js
+++ src/Components/CraftCategories.js
@@ -3,101 +3,128 @@ import styled from "styled-components";
 
 const CraftCategories = () => {
   return (
-    <CategorySection>
-      <CategoryBox style={{ background: "blue" }}>
-        <CategoryIcon
-          src={require(`../assets/categoryIcons/CraftCircleIcon-08.png`)}
-          alt="crafts-in-action"
-        />
-        <CategoryName>sewing/knitting</CategoryName>
-      </CategoryBox>
-      <CategoryBox style={{ background: "blue" }}>
-        <CategoryIcon
-          src={require(`../assets/categoryIcons/CraftCircleIcon-09.png`)}
-          alt="crafts-in-action"
-        />
-        <CategoryName>paper</CategoryName>
-      </CategoryBox>
-      <CategoryBox style={{ background: "blue" }}>
-        <CategoryIcon
-          src={require(`../assets/categoryIcons/CraftCircleIcon-10.png`)}
-          alt="crafts-in-action"
-        />
-        <CategoryName>baking</CategoryName>
-      </CategoryBox>
-      <CategoryBox style={{ background: "blue" }}>
-        <CategoryIcon
-          src={require(`../assets/categoryIcons/CraftCircleIcon-11.png`)}
-          alt="crafts-in-action"
-        />
-        <CategoryName>jewelry</CategoryName>
-      </CategoryBox>
-      <CategoryBox style={{ background: "blue" }}>
-        <CategoryIcon
-          src={require(`../assets/categoryIcons/CraftCircleIcon-12.png`)}
-          alt="crafts-in-action"
-        />
-        <CategoryName>scrapbook/floral</CategoryName>
-      </CategoryBox>
-      <CategoryBox style={{ background: "blue" }}>
-        <CategoryIcon
-          src={require(`../assets/categoryIcons/CraftCircleIcon-13.png`)}
-          alt="crafts-in-action"
-        />
-        <CategoryName>painting</CategoryName>
-      </CategoryBox>
-      <CategoryBox style={{ background: "blue" }}>
-        <CategoryIcon
-          src={require(`../assets/categoryIcons/CraftCircleIcon-14.png`)}
-          alt="crafts-in-action"
-        />
-        <CategoryName>drawing</CategoryName>
-      </CategoryBox>
-      <CategoryBox style={{ background: "blue" }}>
-        <CategoryIcon
-          src={require(`../assets/categoryIcons/CraftCircleIcon-15.png`)}
-          alt="crafts-in-action"
-        />
-        <CategoryName>wood working</CategoryName>
-      </CategoryBox>
-      <CategoryBox style={{ background: "blue" }}>
-        <CategoryIcon
-          src={require(`../assets/categoryIcons/CraftCircleIcon-16.png`)}
-          alt="crafts-in-action"
-        />
-        <CategoryName>pottery</CategoryName>
-      </CategoryBox>
-      <CategoryBox style={{ background: "blue" }}>
-        <CategoryIcon
-          src={require(`../assets/categoryIcons/CraftCircleIcon-17.png`)}
-          alt="crafts-in-action"
-        />
-        <CategoryName>other</CategoryName>
-      </CategoryBox>
-    </CategorySection>
+    <>
+      <SectionTitle>Categories:</SectionTitle>
+      <CategorySection>
+        <CategoryBox style={{ background: "blue" }}>
+          <CategoryIcon
+            src={require(`../assets/categoryIcons/CraftCircleIcon-08.png`)}
+            alt="crafts-in-action"
+          />
+          <CategoryName>sewing/knitting</CategoryName>
+        </CategoryBox>
+        <CategoryBox style={{ background: "blue" }}>
+          <CategoryIcon
+            src={require(`../assets/categoryIcons/CraftCircleIcon-09.png`)}
+            alt="crafts-in-action"
+          />
+          <CategoryName>paper</CategoryName>
+        </CategoryBox>
+        <CategoryBox style={{ background: "blue" }}>
+          <CategoryIcon
+            src={require(`../assets/categoryIcons/CraftCircleIcon-10.png`)}
+            alt="crafts-in-action"
+          />
+          <CategoryName>baking</CategoryName>
+        </CategoryBox>
+        <CategoryBox style={{ background: "blue" }}>
+          <CategoryIcon
+            src={require(`../assets/categoryIcons/CraftCircleIcon-11.png`)}
+            alt="crafts-in-action"
+          />
+          <CategoryName>jewelry</CategoryName>
+        </CategoryBox>
+        <CategoryBox style={{ background: "blue" }}>
+          <CategoryIcon
+            src={require(`../assets/categoryIcons/CraftCircleIcon-12.png`)}
+            alt="crafts-in-action"
+          />
+          <CategoryName>scrapbook/floral</CategoryName>
+        </CategoryBox>
+        <CategoryBox style={{ background: "blue" }}>
+          <CategoryIcon
+            src={require(`../assets/categoryIcons/CraftCircleIcon-13.png`)}
+            alt="crafts-in-action"
+          />
+          <CategoryName>painting</CategoryName>
+        </CategoryBox>
+        <CategoryBox style={{ background: "blue" }}>
+          <CategoryIcon
+            src={require(`../assets/categoryIcons/CraftCircleIcon-14.png`)}
+            alt="crafts-in-action"
+          />
+          <CategoryName>drawing</CategoryName>
+        </CategoryBox>
+        <CategoryBox style={{ background: "blue" }}>
+          <CategoryIcon
+            src={require(`../assets/categoryIcons/CraftCircleIcon-15.png`)}
+            alt="crafts-in-action"
+          />
+          <CategoryName>wood working</CategoryName>
+        </CategoryBox>
+        <CategoryBox style={{ background: "blue" }}>
+          <CategoryIcon
+            src={require(`../assets/categoryIcons/CraftCircleIcon-16.png`)}
+            alt="crafts-in-action"
+          />
+          <CategoryName>pottery</CategoryName>
+        </CategoryBox>
+        <CategoryBox style={{ background: "blue" }}>
+          <CategoryIcon
+            src={require(`../assets/categoryIcons/CraftCircleIcon-17.png`)}
+            alt="crafts-in-action"
+          />
+          <CategoryName>other</CategoryName>
+        </CategoryBox>
+      </CategorySection>
+    </>
   );
 };
 
 export default CraftCategories;
 
-const CategorySection = styled.section``;
+const SectionTitle = styled.h1`
+  align-self: start;
+  color: purple;
+  font-size: 35px;
+  font-weight: 700;
+  margin-left: 7.5vw;
+`;
+
+const CategorySection = styled.section`
+  margin-top: 5vh;
+  width: 90vw;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+`;
 
 const CategoryBox = styled.div`
-  width: 200px;
-  height: 200px;
+  align-self: center;
+  width: 190px;
+  height: 190px;
+  margin: 5%;
   border-radius: 25px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align items: column;
+  align-items: center;
+  cursor: pointer;
+  &: hover {
+    cursor: pointer;
+    color: #e3976e;
+    box-shadow: 5px 5px 0px #e3976e;
 `;
 
 const CategoryIcon = styled.img`
-  width: 180px;
-  height: 180px;
+  width: 80%;
+  height: 80%;
 `;
 
 const CategoryName = styled.p`
   font-weight: 700;
   color: white;
+  margin: 0;
 `;
