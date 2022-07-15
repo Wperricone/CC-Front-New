95e9b1c3fd66d4d5eeb692e42824d6984804839a
Joe Rupp <joerpp8@gmail.com>
2022-07-15 16:47:44 -0600

    Add profile page structure
    Structure for profile page has been added along with the styled component setup

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -4,21 +4,37 @@ import Button from "./Button";
 
 const UserProfile = () => {
   return (
-    <section className="user-profile">
-      <section className="bio">
-        <img className="profile-picture" src="" alt="crafter profile picture" />
-        <h2>Name</h2>
-        <h3>Contact info/ Location</h3>
-      </section>
-      <section className="available-items">
-        <h3>Supplies I am Offering</h3>
+    <UserProfileSection className="user-profile">
+      <BannerImg></BannerImg>
+      <ProfilePicture></ProfilePicture>
+      <ProfileName></ProfileName>
+      <ProfileEmail></ProfileEmail>
+      <CraftDisplaySection>
+        <SectionHeader>Crafts I'm Offering</SectionHeader>
         <Button name={"Add Craft"} link={"/contribution"} />
-        <div className="user-cards">craft cards would go here</div>
-      </section>
-    </section>
+      </CraftDisplaySection>
+      <CraftDisplaySection>
+        <SectionHeader>Borrowed Supplies</SectionHeader>
+      </CraftDisplaySection>
+      <Button name={"Log Out"} link={"/login"} />
+    </UserProfileSection>
   );
 };
 
 export default UserProfile;
 
 // wireframe shows borrowed crafts, but it is not explicity mentioned in MVP. If we move forward with this, it will have a similar structure to the available section.
+
+const UserProfileSection = styled.section``;
+
+const BannerImg = styled.img``;
+
+const ProfilePicture = styled.img``;
+
+const ProfileName = styled.h1``;
+
+const ProfileEmail = styled.h2``;
+
+const CraftDisplaySection = styled.section``;
+
+const SectionHeader = styled.section``;
