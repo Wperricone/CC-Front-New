a6728452b9572092202e3feeb80e40194eb953e8
Whitney Perricone <whitney.perricone@gmail.com>
2022-07-14 19:14:51 -0600

    Add Categories to Form
    Allow users to input data necessary for a post

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,6 +8,20 @@ const Form = () => {
             <input type="text" placeholder="item name"></input>
             <input type="text" placeholder="amount"></input>
             <input type="text" placeholder="description"></input>
+            <div className="dropdown">
+             <select id="myDropdown" className="dropdown-content">
+               <option value="#">Sewing/Knitting</option>
+               <option value="#">Paper</option>
+               <option value="#">Baking</option>
+               <option value="#">Jewelry</option>
+               <option value="#">Scrapbook/Floral</option>
+               <option value="#">Painting</option>
+               <option value="#">Drawing</option>
+               <option value="#">Wood Working</option>
+               <option value="#">Pottery</option>
+               <option value="#">Other</option>
+             </select>
+            </div>            
             <button>Add Item</button>
         </form>
     )
@@ -16,3 +30,4 @@ const Form = () => {
 export default Form
 
 // potentially add a type to the form? would be useful if we choose to filter 
+//need to , available will default to True, and user ID will be current use, id will be assigned, potentially a picture upload (post-mvp) 
