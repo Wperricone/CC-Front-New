42b40d7afe0506fdf3defe0fb1a91e953d1bea02
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-07-17 09:42:59 -0600

    Create LoadingPage component
    Component built to communicate to user that the application data should be populated soon

--- src/Components/LoadingPage.js
+++ src/Components/LoadingPage.js
@@ -1,2 +1,28 @@
-import Lottie from "lottie-react";
 import React from "react";
+import Lottie from "lottie-react";
+import styled from "styled-components";
+import colors from "../constants/colors";
+
+const LoadingPage = () => {
+   return  (
+    <LoadingPageContainer>
+        <LoadingMessage className="loading-message">Please wait, our application is loading ....</LoadingMessage>
+            <Lottie className="loading-animation" style={{height: "800px"}} animationData={require("../assets/animations/drawing.json")} loop={true} />
+    </LoadingPageContainer>
+   )
+};
+
+export default LoadingPage;
+
+const LoadingMessage = styled.h2`
+   color: ${colors.craftPurple};
+   font-size: 40px;
+   padding: 40px;
+   padding-top: 60px;
+`
+
+const LoadingPageContainer = styled.section`
+    background-color: #F4CEBD;
+    height: 100vh;
+    width: 100vw;
+`
