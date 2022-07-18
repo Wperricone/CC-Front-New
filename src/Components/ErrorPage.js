16fdd3834c434c93c438d77283377bf81f2e5ce5
Whitney Perricone <whitney.perricone@gmail.com>
2022-07-18 16:32:10 -0600

    Add Cypress for error page

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -3,12 +3,11 @@ import styled from "styled-components";
 import Lottie from "lottie-react";
 import colors from "../constants/colors";
 
-
 const ErrorPage = () => {
   return  (
    <ErrorPageContainer className="error-page">
        <ErrorMessage className="error-message">Oh no, something went wrong! Please try again....</ErrorMessage>
-           <Lottie className="error-animation" style={{height: "700px"}} animationData={require("../assets/animations/painting-girl.json")} loop={true} />
+           <Lottie className="error-animation" style={{height: "700px"}} animationData={require("../assets/animations/painting-girl.json")} loop={true} />        
    </ErrorPageContainer>
   )
 };
