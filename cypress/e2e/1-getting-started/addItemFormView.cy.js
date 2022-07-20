6a1c6a5006e9863c2da1a047309af33c69a713a8
Whitney Perricone <whitney.perricone@gmail.com>
2022-07-20 15:07:10 -0600

    Begin stubbing for add-item-form

--- cypress/e2e/1-getting-started/addItemFormView.cy.js
+++ cypress/e2e/1-getting-started/addItemFormView.cy.js
@@ -1,10 +1,139 @@
-describe('Craft Circle landing page view', () => {
-  beforeEach(() => {
-    // cy.intercept("craft-circle-be.herokuapp.com/graphql", {fixture: "userProfile.json"})
-    cy.visit("http://localhost:3000")
-  });
+import {aliasQuery, aliasMutation, hasOperationName} from '../utils/graphql-test-utils';
 
-  it.skip('Should have three inputs', () => {
+describe('Add Item Form Page', () => {
+beforeEach(() => {
+
+  
+  cy.intercept(
+    "POST",
+    "https://craft-circle-be.herokuapp.com/graphql",
+    (req) => {
+      const { body } = req;
+      aliasQuery(req, "getItems");
+      if (hasOperationName(req, "getItems")) {
+        req.alias = "gqlgetItemsQuery";
+        req.reply((res) => {
+          res.body.data.items = [
+            {
+              amount: 5,
+              available: "false",
+              category: "Paper",
+              description:
+                "Austin fixie letterpress hashtag brunch normcore chambray.",
+              id: "1",
+              name: "Refreshed-copper Awesome Rubber Chair",
+              status: "Give",
+              user: {
+                email: "collins_alvin@example.com",
+                id: "1",
+                name: "Alvin Collins",
+                __typename: "User",
+              },
+              __typename: "Item",
+            },
+
+            {
+              amount: 1,
+              available: "true",
+              category: "Sewing/Knitting",
+              description: "Purple yarn.",
+              id: "2",
+              name: "Purple Yarn",
+              status: "Give",
+              user: {
+                email: "collins_alvin@example.com",
+                id: "1",
+                name: "Alvin Collins",
+                __typename: "User",
+              },
+              __typename: "Item",
+            },
+
+            {
+              amount: 1,
+              available: "true",
+              category: "Paper",
+              description: "New package of color construction paper.",
+              id: "3",
+              name: "Construction Paper",
+              status: "Give",
+              user: {
+                email: "joe@example.com",
+                id: "2",
+                name: "Crafty Joe",
+                __typename: "User",
+              },
+              __typename: "Item",
+            },
+          ];
+        });
+      } else {
+        console.log("Not Running");
+      }
+    }
+  );
+  
+  
+  cy.visit('http://localhost:3000');
+  // cy.get(".category-section").should("exist");
+  //cy.get(".item-card").should("have.length", 3);
+  cy.get(".nav-button").eq(2).contains("Profile").click();
+  cy.url().should("eq", "http://localhost:3000/profile");
+  cy.get("button").eq(3).click();
+  cy.url().should("eq", "http://localhost:3000/login");
+  cy.get('input').eq(0).click().type('tanna.schmeler@example.net');
+  cy.get('input').eq(1).click().type('password');
+  cy.intercept(
+    "POST",
+    "https://craft-circle-be.herokuapp.com/graphql",
+    (req) => {
+      const { body } = req;
+      aliasQuery(req, "getAUser");
+      if (hasOperationName(req, "getAUser")) {
+        req.alias = "gqlgetAUserQuery";
+        req.reply((res) => {
+          res.body.data.details = {
+            email: "tanna.schmeler@example.net",
+            id: "9",
+            items: [
+              {
+                amount: 5,
+                available: "false",
+                category: "Paper",
+                description:
+                  "Austin fixie letterpress hashtag brunch normcore chambray.",
+                id: "1",
+                name: "Refreshed-copper Awesome Rubber Chair",
+                status: "Give",
+                __typename: "Item",
+              },
+              {
+                amount: 1,
+                available: "true",
+                category: "Sewing/Knitting",
+                description: "Purple yarn.",
+                id: "2",
+                name: "Purple Yarn",
+                status: "Give",
+                __typename: "Item",
+              },
+            ],
+            name: "Tanna Test",
+            __typename: "User",
+          };
+        });
+      } else {
+        console.log("Not Running");
+      }
+    }
+  );
+  cy.get('button').eq(3).click().wait(2000);
+  //cy.visit('http://localhost:3000');
+  // cy.get('button').contains('Add Craft').click();
+});
+
+  it('Should have three inputs', () => {
+      cy.get('button').contains('Add Craft').click();
     cy.get('.add-item')
     cy.get('input')
     .eq(0)
